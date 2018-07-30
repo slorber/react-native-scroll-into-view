5e1360625b7016cb1df5df0810077e2a1bb5b750
slorber <lorber.sebastien@gmail.com>
2018-07-30 10:04:16 +0200

    fix home screen

--- examples/src/screens/HomeScreen.js
+++ examples/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Button, Centered} from "components/Components";
+import { Text } from 'react-native';
+import { Button, Centered } from 'components/Components';
 
 
 class HomeScreen extends React.Component {
@@ -10,20 +10,25 @@ class HomeScreen extends React.Component {
 
   render() {
     return (
-      <Centered style={{flex: 1, padding: 20}}>
-        <Text style={{fontSize: 20}}>react-native-scroll-into-view</Text>
-        <Text style={{fontSize: 14, marginTop: 50, textAlign: "center"}}>
+      <Centered style={{ flex: 1, padding: 20 }}>
+        <Text style={{ fontSize: 20 }}>react-native-scroll-into-view</Text>
+        <Text style={{ fontSize: 14, marginTop: 50, textAlign: 'center' }}>
           This is the demo app for react-native-scroll-into-view
         </Text>
         <Button
-          style={{marginTop: 50}}
-          onPress={() => this.props.navigation.navigate("Basic")}>
-          Open basic example
+          style={{ marginTop: 50 }}
+          onPress={() => this.props.navigation.navigate('Basic')}>
+          Basic
         </Button>
         <Button
-          style={{marginTop: 50}}
-          onPress={() => this.props.navigation.navigate("ScrollToNext")}>
-          Open Scroll-to-next example
+          style={{ marginTop: 50 }}
+          onPress={() => this.props.navigation.navigate('ScrollToNext')}>
+          Scroll-to-next
+        </Button>
+        <Button
+          style={{ marginTop: 50 }}
+          onPress={() => this.props.navigation.navigate('Formik')}>
+          Formik
         </Button>
       </Centered>
     );
