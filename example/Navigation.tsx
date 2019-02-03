76cd390fd4b9b78ea49b786375eeb2366041d316
slorber <lorber.sebastien@gmail.com>
2019-02-03 19:08:16 +0100

    add sections example

--- example/Navigation.tsx
+++ example/Navigation.tsx
@@ -4,6 +4,29 @@ import BasicScreen from './screens/BasicScreen';
 import ScrollToNextScreen from './screens/ScrollToNextScreen';
 import HomeScreen from './screens/HomeScreen';
 import FormikScreen from './screens/FormikScreen';
+import SectionsScreen from './screens/SectionsScreen';
+
+const Routes = {
+  Home: {
+    screen: HomeScreen,
+  },
+  Basic: {
+    screen: BasicScreen,
+  },
+  ScrollToNext: {
+    screen: ScrollToNextScreen,
+  },
+  Formik: {
+    screen: FormikScreen,
+  },
+  Sections: {
+    screen: SectionsScreen,
+  },
+};
+
+export const HomeScreenNames = Object.keys(Routes).filter(
+  name => name !== 'Home',
+);
 
 const Navigation = createStackNavigator(
   {
@@ -19,9 +42,12 @@ const Navigation = createStackNavigator(
     Formik: {
       screen: FormikScreen,
     },
+    Sections: {
+      screen: SectionsScreen,
+    },
   },
   {
-    // initialRouteName: 'Formik',
+    initialRouteName: 'Sections',
   },
 );
 
--- example/screens/HomeScreen.tsx
+++ example/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { Button, Centered } from '../components/Components';
+import { HomeScreenNames } from '../Navigation';
 
 
 class HomeScreen extends React.Component<{navigation: {navigate: any}}> {
@@ -12,24 +13,17 @@ class HomeScreen extends React.Component<{navigation: {navigate: any}}> {
     return (
       <Centered style={{ flex: 1, padding: 20 }}>
         <Text style={{ fontSize: 20 }}>react-native-scroll-into-view</Text>
-        <Text style={{ fontSize: 14, marginTop: 50, textAlign: 'center' }}>
+        <Text style={{ fontSize: 14, marginTop: 20, textAlign: 'center' }}>
           This is the demo app for react-native-scroll-into-view
         </Text>
-        <Button
-          style={{ marginTop: 50 }}
-          onPress={() => this.props.navigation.navigate('Basic')}>
-          Basic
-        </Button>
-        <Button
-          style={{ marginTop: 50 }}
-          onPress={() => this.props.navigation.navigate('ScrollToNext')}>
-          Scroll-to-next
-        </Button>
-        <Button
-          style={{ marginTop: 50 }}
-          onPress={() => this.props.navigation.navigate('Formik')}>
-          Formik
-        </Button>
+        {HomeScreenNames.map(screenName => (
+          <Button
+            key={screenName}
+            style={{ marginTop: 20 }}
+            onPress={() => this.props.navigation.navigate(screenName)}>
+            {screenName}
+          </Button>
+        ))}
       </Centered>
     );
   }
